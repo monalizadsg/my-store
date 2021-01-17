9b6d74638c9b93c70a9a6ef53a7d899a71a74618
Monaliza Dasig <monalizadasig@gmail.com>
2021-01-17 21:01:19 +0800

    Fixed routing; removed private and public router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import AppRouter from "./app/AppRouter";
+import { Route, Switch } from "react-router-dom";
+import { CartContextProvider } from "./global/CartContext";
 import { ProductsContextProvider } from "./global/ProductsContext";
+import { UserContextProvider } from "./global/UserContext";
+import Products from "./components/Products";
+import AddProduct from "./components/AddProduct";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <React.Fragment>
       <ProductsContextProvider>
-        <AppRouter />
+        <CartContextProvider>
+          <UserContextProvider>
+            <Switch>
+              <Route exact component={Products} path='/' />
+              <Route component={AddProduct} path='/add-product' />
+              <Route component={Login} path='/login' />
+              <Route component={Signup} path='/signup' />
+              <Route component={Cart} path='/my-cart' />
+            </Switch>
+          </UserContextProvider>
+        </CartContextProvider>
       </ProductsContextProvider>
     </React.Fragment>
   );
