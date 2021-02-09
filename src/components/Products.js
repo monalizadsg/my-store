b9e61b36d27b72e0c2638798d7da0cf95c41fb38
Monaliza Dasig <monalizadasig@gmail.com>
2021-02-09 16:34:06 +0800

    Handle cart functions when adding products to cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,66 +1,90 @@
 import React, { useContext } from "react";
 import { ProductsContext } from "./../global/ProductsContext";
-import {
-  Card,
-  Typography,
-  CardContent,
-  CardMedia,
-  CardActions,
-} from "@material-ui/core";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { Card, Typography, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import "./Products.scss";
+import Header from "./Header";
+import { CartContext } from "./../global/CartContext";
+import Loading from "./Loading";
+import { ToastContext } from "../global/ToastContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
   media: {
-    height: 100,
-    paddingTop: "75%", // 16:9
+    height: 60,
+    paddingTop: "70%", // 16:9
   },
 }));
 
 const Products = () => {
-  const { products } = useContext(ProductsContext);
   const classes = useStyles();
+  const { products } = useContext(ProductsContext);
+  const { dispatch, shoppingCart } = useContext(CartContext);
+  const { showToast } = useContext(ToastContext);
 
-  console.log(products);
+  const handleAddToCart = (item) => {
+    console.log(shoppingCart);
+    const isItemAdded = shoppingCart.find((product) => product.id === item.id);
+
+    if (isItemAdded) {
+      showToast("Item is already in your cart!", "warning");
+    } else {
+      dispatch({
+        type: "ADD_TO_CART",
+        id: item.id,
+        product: item,
+      });
+      showToast("Item added to cart!", "success");
+    }
+  };
 
   return (
-    <div className='products-wrapper'>
-      {products.length !== 0 && <Typography variant='h4'>Products</Typography>}
-      <div className='card-container'>
-        {products.length === 0 && (
-          <div>slow internet...no products to display</div>
+    <>
+      <Header />
+      <div className='products-wrapper'>
+        {products.length !== 0 && (
+          <Typography variant='h5'>Products</Typography>
         )}
-        {products.map((product) => (
-          <Card className={`card ${classes.root}`}>
-            <CardMedia
-              className={classes.media}
-              // component='img'
-              // height='140'
-              image={product.img}
-              title={product.name}
-            />
-            <CardContent>
-              <div>
+        <div className='card-container'>
+          {products.length === 0 && (
+            <div className='loading'>
+              <Loading isLoading='true' />
+            </div>
+          )}
+          {products.map((product) => (
+            <div key={product.name} className='card'>
+              <Card className={classes.root}>
+                <CardMedia
+                  className={classes.media}
+                  image={product.img}
+                  title={product.name}
+                />
+                <div className='content'>
+                  <div className='add-to-cart'>
+                    <button
+                      className='add-to-cart-btn'
+                      onClick={() => handleAddToCart(product)}
+                    >
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              </Card>
+              <div className='product-details'>
                 <Typography variant='body2' color='textSecondary' component='p'>
                   {product.name}
                 </Typography>
-                <Typography variant='body2' color='textSecondary' component='p'>
+                <Typography variant='body2' component='p'>
                   {`P${product.price}.00`}
                 </Typography>
               </div>
-              <div>
-                <ShoppingCartIcon />
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-    // <h1>Products</h1>
+    </>
   );
 };
 
